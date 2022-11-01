e9a2bed968ecad9767198e0b8074b73a61931b72
Fabin๏ <githubuseronly@gmail.com>
2022-11-01 13:21:01 +0530

    Sexy Repo

---
+++ AnonX/utils/inline/start.py
@@ -0,0 +1,58 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import config
+from AnonX import app
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="🥺 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴩ 🥺",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="ʜᴇʟᴩ",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="sᴇᴛᴛɪɴɢs", callback_data="settings_helper"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴍᴀɪɴᴛᴀɪɴᴇʀ", user_id=OWNER),
+            InlineKeyboardButton(
+                text="sᴜᴩᴩᴏʀᴛ", url=config.SUPPORT_GROUP
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="ʜᴇʟᴩ", callback_data="settings_back_helper"
+            ),
+            InlineKeyboardButton(text="ᴀʙᴏᴜᴛ", callback_data="cb_about")
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴄʜᴀɴɴᴇʟ", url=config.SUPPORT_CHANNEL
+            ),
+            InlineKeyboardButton(text="ᴍᴀɪɴᴛᴀɪɴᴇʀ", user_id=OWNER)
+        ],
+     ]
+    return buttons
