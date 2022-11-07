61dec4dbaaeba453f6a1db950d51d5dac1046a56
SPARTANBOY xD <114400345+SPARTENX-OP@users.noreply.github.com>
2022-11-07 00:56:44 -0800

    Update song.py

--- AnonX/utils/inline/song.py
+++ AnonX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_GROUP}",
+                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"https://t.me/FallenXMusic",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
