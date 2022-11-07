45a28343b2054407b084c5cdb300888c451b2704
SPARTANBOY xD <114400345+SPARTENX-OP@users.noreply.github.com>
2022-11-07 00:55:33 -0800

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -10,24 +10,24 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🥀🎼ADD ME TO YOUƦ GƦOUᴩ🎼🥀",
+                text="🎼ADD ME TO YOUƦ GƦOUᴩ🎼",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🥀🎼ʜᴇʟᴘ🎼🥀",
+                text="🎼ʜᴇʟᴘ🎼",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="🥀🎼ꜱᴇᴛᴛɪɴɢꜱ🎼🥀", callback_data="settings_helper"
+                text="🎼ꜱᴇᴛᴛɪɴɢꜱ🎼", callback_data="settings_helper"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🥀🎼ᴏᴡɴᴇʀ🎼🥀", user_id=OWNER),
+                text="🎼ᴏᴡɴᴇʀ🎼", user_id=OWNER),
             InlineKeyboardButton(
-                text="🥀🎼ꜱᴜᴘᴘᴏʀᴛ🎼🥀", url=config.SUPPORT_GROUP
+                text="🎼ꜱᴜᴘᴘᴏʀᴛ🎼", url=config.SUPPORT_GROUP
             ),
         ],
      ]
@@ -38,21 +38,21 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🥀🎼ADD ME TO YOUƦ GƦOUᴩ🎼🥀",
+                text="🎼ADD ME TO YOUƦ GƦOUᴩ🎼",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🥀🎼ʜᴇʟᴘ🎼🥀", callback_data="settings_back_helper"
+                text="🎼ʜᴇʟᴘ🎼", callback_data="settings_back_helper"
             ),
-            InlineKeyboardButton(text="ᴀʙᴏᴜᴛ", callback_data="cb_about")
+            InlineKeyboardButton(text="🎼ᴅᴇᴠᴇʟᴏᴘᴇʀ🎼", url="https://t.me/ll_ll_LegendHacker_IN_ll_ll")
         ],
         [
             InlineKeyboardButton(
-                text="🥀🎼ᴄʜᴀɴɴᴇʟ🎼🥀", url=config.SUPPORT_CHANNEL
+                text="🎼ᴄʜᴀɴɴᴇʟ🎼", url=config.SUPPORT_CHANNEL
             ),
-            InlineKeyboardButton(text="🥀🎼ᴏᴡɴᴇʀ🎼🥀", user_id=OWNER)
+            InlineKeyboardButton(text="🎼ᴏᴡɴᴇʀ🎼", user_id=OWNER)
         ],
      ]
     return buttons
